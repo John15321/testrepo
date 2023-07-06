354e24b328ca83cd7b429d054306f97abfa4a353
Jan Bronicki <janbronicki@gmail.com>
2023-07-07 01:10:34 +0200

    feat: Jesussssssss

--- release.config.js
+++ release.config.js
@@ -1,22 +1,19 @@
 const config = {
-  branches: ['main'],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
     [
       "@semantic-release/changelog",
       {
-        "changelogFile": "docs/CHANGELOG.md"
+        "changelogFile": "CHANGELOG.md"
       }
     ],
-    ["@semantic-release/git", {
-      // "assets": ["dist/*.js", "dist/*.js.map"],
-      "assets": ["kek.txt", "CHANGELOG.md"],
-      "message": "chore(release 🚀): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-    }],
-    'semantic-release-github-actions-tags',
-    '@semantic-release/github',
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["CHANGELOG.md"]
+      }
+    ]
   ]
 };
-
 module.exports = config;
