4dbcd35363babbf5ebc3002787f4f139dc830665
Jan Bronicki <janbronicki@gmail.com>
2023-07-07 00:41:34 +0200

    feat: Jessus christ

--- release.config.js
+++ release.config.js
@@ -3,20 +3,20 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ["@semantic-release/git", {
-      // "assets": ["dist/*.js", "dist/*.js.map"],
-      "assets": ["kek.txt", "CHANGELOG.md"],
-      "message": "chore(release 🚀): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      "changelogFile": "CHANGELOG.md",
-    }],
-    '@semantic-release/github',
-    'semantic-release-github-actions-tags',
     [
       "@semantic-release/changelog",
       {
         "changelogFile": "docs/CHANGELOG.md"
       }
     ],
+    ["@semantic-release/git", {
+      // "assets": ["dist/*.js", "dist/*.js.map"],
+      "assets": ["kek.txt", "CHANGELOG.md"],
+      "message": "chore(release 🚀): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+    }],
+    '@semantic-release/github',
+    'semantic-release-github-actions-tags',
+
   ]
 };
 
